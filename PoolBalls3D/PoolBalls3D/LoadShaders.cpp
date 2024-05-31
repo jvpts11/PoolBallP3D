dd0781497bd0e9568a6cada7036379800a3cf9f9
Zeet76 <a23496@alunos.ipca.pt>
2024-05-31 21:31:01 +0100

    LoadShaders comments added

--- PoolBalls3D/PoolBalls3D/LoadShaders.cpp
+++ PoolBalls3D/PoolBalls3D/LoadShaders.cpp
@@ -4,7 +4,7 @@
  * Authors: João Tavares nº 21871, Diogo Silva nº 22369, Ademar Valente nº 23155, José Lourenço nº23496
  * Date: 31/05/2024
  * File: LoadShaders.cpp
- * Description: Implementation of the shader loading functions.
+ * Description: Implementation of the shader loading and reading functions.
  */
 //-----------------------------------------------------------------------------------------------------
 #pragma region Imports
@@ -22,20 +22,28 @@
 #pragma region Functions
 
 GLuint loadShaders(shaderInfo* shaders) {
+
+	// Check if the shader is not NULL
 	if (shaders == NULL)
 	{
 		return 0;
 	}
 
+	// Create the program
 	GLuint program = glCreateProgram();
 
 	for (shaderInfo* entry = shaders; entry->type != GL_NONE; entry++)
 	{
+		// Create the shader with thes specified type
 		GLuint shader = glCreateShader(entry->type);
 
+		// Read the shader code from the file
 		const char* shaderCode = readShader(entry->filename);
+
+		// Check for problems reading the shader code
 		if (shaderCode == NULL)
 		{
+			// Delete the shaders
 			for (shaderInfo* entry = shaders; entry->type != GL_NONE; entry++)
 			{
 				glDeleteShader(entry->shader);
@@ -43,13 +51,20 @@ GLuint loadShaders(shaderInfo* shaders) {
 			return 0;
 		}
 
+		// Set the shader source code
 		glShaderSource(shader, 1, &shaderCode, NULL);
+
+		// Free the memory allocated for the shader code
 		delete[] shaderCode;
 
+		// Compile the shader
 		glCompileShader(shader);
 
+		// Get the compilation status
 		GLint compiled;
 		glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+
+		// Check if the shader compiled successfully
 		if (!compiled)
 		{
 			GLint length;
@@ -60,6 +75,7 @@ GLuint loadShaders(shaderInfo* shaders) {
 			std::cerr << "Shader compilation failed: " << log << "." << std::endl;
 			delete[] log;
 
+			// Delete the shaders if the shader compilation failed
 			for (shaderInfo* entry = shaders; entry->type != GL_NONE; entry++)
 			{
 				glDeleteShader(entry->shader);
@@ -68,14 +84,18 @@ GLuint loadShaders(shaderInfo* shaders) {
 			return 0;
 		}
 
+		// Attach the shader to the program
 		glAttachShader(program, shader);
-		//entry->shader = shader;
 	}
 
+	// Link the program to OpenGL context
 	glLinkProgram(program);
 
+	// Get the linking status
 	GLint linked;
 	glGetProgramiv(program, GL_LINK_STATUS, &linked);
+
+	// Check if the program linked successfully
 	if (!linked)
 	{
 		GLint length;
@@ -86,6 +106,7 @@ GLuint loadShaders(shaderInfo* shaders) {
 		std::cerr << "Shader linking failed: " << log << "." << std::endl;
 		delete[] log;
 
+		// Delete the shaders if the program linking failed
 		for (shaderInfo* entry = shaders; entry->type != GL_NONE; entry++)
 		{
 			glDeleteShader(entry->shader);
@@ -98,22 +119,33 @@ GLuint loadShaders(shaderInfo* shaders) {
 }
 
 static const char* readShader(const char* filename) {
+
+	// Open the file in binary mode and at the end
 	std::ifstream file(filename, std::ifstream::ate | std::ifstream::binary);
+
+	// Check if the file was opened successfully
 	if (!file.is_open())
 	{
 		std::cerr << "Failed to open file: " << filename << "." << std::endl;
 		return NULL;
 	}
 
+	// Get the size of the file
 	std::streampos size = file.tellg();
 
+	// Reposition the file pointer to the beginning
 	file.seekg(0, std::ifstream::beg);
 
+	// Allocate memory for the file content
 	char* buffer = new char[int(size) + 1];
 
+	// Read the file content
 	file.read(buffer, size);
+
+	// Close the reading string
 	buffer[size] = '\0';
 
+	// Close the file
 	file.close();
 
 	return buffer;
--- PoolBalls3D/PoolBalls3D/LoadShaders.h
+++ PoolBalls3D/PoolBalls3D/LoadShaders.h
@@ -4,18 +4,23 @@
  * Authors: João Tavares nº 21871, Diogo Silva nº 22369, Ademar Valente nº 23155, José Lourenço nº23496
  * Date: 31/05/2024
  * File: LoadShaders.h
- * Description: Signatures related to the implementation of the shader loading functions.
+ * Description: Signatures related to the implementation of the shader loading and reading functions.
  */
 //-----------------------------------------------------------------------------------------------------
 #pragma once
 
 #include <GL\gl.h>
 
+// Structure that represents the shader information
 typedef struct {
 	GLenum type;
 	const char* filename;
 	GLuint shader;
 }shaderInfo;
 
+// Function signatures
+	// Function that loads the shaders
 GLuint loadShaders(shaderInfo* shaders);
+
+	// Function that reads the shader
 static const char* readShader(const char* filename);
